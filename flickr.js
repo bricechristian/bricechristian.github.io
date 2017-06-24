89355475c799bcdc0e19001d0ea3a458f4b35cb8
Brice Christian <bricechristian@bricemac.attlocal.net>
2017-06-24 01:24:07 -0400

    Update to background images, validation and styles

--- flickr.js
+++ flickr.js
@@ -1,44 +1,3 @@
-
-
-
-// function render(data) {
-//     var photos = data.items;
-//     console.log(data);
-//     for (var i = 0; i < photos.length; i++) {
-//         // var thumbURL = photos[i].[???];
-//         var photoURL = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_m.jpg';
-//         $('.results').append('<li>' + '<img src= "' + thumbURL + '" >').wrap('<a href="' + photoURL + '" >');
-//     }
-// }
-
-
-
-
-// function init() {
-
-//     var search = $('.query').val();
-//     var apiKey = '9212a18bf4d01a1af48dad5c36f655a5';
-//     var photoID = 'item.id'
-//     var data = {
-//         url: 'https://api.flickr.com/services/rest/?&amp;method=flickr.photos.search',
-//         data: {
-//             apiKey: '9212a18bf4d01a1af48dad5c36f655a5',
-//             dataType: 'jsonp',
-//             tags: search, 
-//             format: 'json'
-//         }
-//     }
-//     $.ajax(data).done(render);
-
-// }
-
-
-// $('button').on('click', function(){
-//   init();
-
-// })
-
-
 $('.container').slideDown("slow");
 $('.results').hide();
 $('.header').hide();
@@ -75,29 +34,8 @@ for(var i = 0; i < photos.length; i ++) {
 }
 
 
-
-
-    // for (var i = 0; i < photos.length; i++) {
-
-
-
-    //     var photo = photos[i];
-    //     var photoURL = 'http://farm' + photo.farm + '.static.flickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_c.jpg';
-    //     var IMG = $('<a href="' + photoURL + '" >')
-    //     .attr('data-lightbox','roadtrip')
-    //     .append('<img class="thumbnail" src="' + photoURL + '" >');
-
-    //     console.log(IMG);
-
-    //     $('.results').show().append('<li>' + IMG);
-
-    // }
-
-
-  if( $('.results').is(':empty') ) {
-   $('.results').text("No results have been found. Try searching for something different!").addClass('error'); 
-  }
 }
+
 
 function search() {
 
@@ -115,6 +53,16 @@ function search() {
             secret: 'd2d8386333f099af'
         }
     };
+
+        var query = $('.query').val();
+        var tags = $('.tags').val();
+        if (query == '' || tags == '') {
+        alert('Please enter a location.')
+          $( this ).abort(function(){
+        $('.itemInput').val('');
+        $('.priceInput').val('');         
+          })
+      }
 
     $.ajax(options).done(render);
   
@@ -186,12 +134,12 @@ $('button').on('click', function() {
 
 $("body").vegas({
     slides: [
-        { src: "http://traveloregon.com/content/uploads/2012/05/cannon-beach-heeb.jpg" },
-        { src: "https://www.canon.com.au/-/media/images/canon/carousels/powershot-g1-x-mark-ii/powershot-g1-x-mark-ii-sample-image-1.ashx" },
-        { src: "http://40.media.tumblr.com/5e6948eabab4b629987c55ee85e6b7c0/tumblr_nvucdgyMof1tgoy1wo3_1280.jpg" },
-        { src: "https://cdn.munplanet.com/storage/uploads/52209627db7c13603b000001/topic/background_image/54c3ba73db7c1355c2002b03/Santorini-Greece-5.jpg" },
-        { src: "http://40.media.tumblr.com/bcc0f5315ce21ddb6db4e47e7eaccb5d/tumblr_o1lxt6T4Uz1qdvvaoo1_1280.jpg" },
-        { src: "http://dgmanila.com/wp-content/uploads/2017/04/swiss_20.jpg" },
-        { src: "https://www.cinema5d.com/wp-content/uploads/2013/06/scot_landscape_4.jpg" }
+        { src: "beach.jpg" },
+        { src: "city.jpg" },
+        { src: "forrest.jpg" },
+        { src: "greece.jpg" },
+        { src: "paris.jpg" },
+        { src: "swiss.jpg" },
+        { src: "valley.jpg" }
     ]
 });
